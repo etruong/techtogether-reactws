0d0fae4c9d6de4befcfef5579618e38a8a3bca98
Elisa <elisat15@uw.edu>
2021-01-18 18:05:36 -0800

    clear out content for workshop

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header';
+
 import { NewsFeed } from './components/NewsFeed';
 import { SideBar } from './components/SideBar';
-import { currentUser, users } from './data/data';
-import { posts } from './data/data';
+import { currentUser, users, posts } from './data/data';
 
 function App(props) {
   const [curUser, setCurrUser] = React.useState(currentUser);
   const [usersData, setUsers] = React.useState(users);
-  const [data, setData] = React.useState(posts);
-
-  const calculateCurrentLikes = () => {
-    let likeCount = 0;
-    data.forEach((post) => {
-      if (post.like) likeCount++;
-    });
-    return likeCount;
-  }
-
-  const calculateCurrentPosts = () => {
-    let postCount = 0;
-    data.forEach((post) => {
-      if (post.userId === curUser.id) postCount++;
-    });
-    return postCount;
-  }
 
   return (<div>
-    <Header />
+    <header>
+        <h1>Crossing Paths</h1>
+        <nav>
+            <ul>
+                <li><i class="fas fa-home"></i></li>
+                <li><i class="fas fa-user-alt"></i></li>
+                <li><i class="fas fa-comment"></i></li>
+                <li><i class="fas fa-tools"></i></li>
+            </ul>
+        </nav>
+    </header>
     <main>
-        <SideBar likeCount={calculateCurrentLikes()} postCount={calculateCurrentPosts()} currentUser={curUser} />
-        <NewsFeed currentUser={curUser} users={usersData} data={data} setData={setData} />
+        <SideBar currentUser={curUser} />
+        <NewsFeed currentUser={curUser} users={usersData} />
     </main>
   </div>);
 }
