dfa675b92d4fa4ffd4abaae6528102011081a66a
Elisa <elisat15@uw.edu>
2021-01-22 20:21:26 -0800

    restart header

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -6,19 +6,3 @@ import {
     faComment,
     faTools,
   } from '@fortawesome/free-solid-svg-icons'
-
-export function Header(props) {
-    return (
-        <header>
-            <h1>Crossing Paths</h1>
-            <nav>
-                <ul>
-                    <li><FontAwesomeIcon icon={faHome} /></li>
-                    <li><FontAwesomeIcon icon={faUserAlt} /></li>
-                    <li><FontAwesomeIcon icon={faComment} /></li>
-                    <li><FontAwesomeIcon icon={faTools} /></li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
